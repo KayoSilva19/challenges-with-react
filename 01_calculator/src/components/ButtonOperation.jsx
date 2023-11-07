fe723505068fb22233f6343afa5a6c050e26de13
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-11-07 09:37:36 -0300

    feat: operations

--- 01_calculator/src/components/ButtonOperation.jsx
+++ 01_calculator/src/components/ButtonOperation.jsx
@@ -1,5 +1,27 @@
-export function ButtonOperation({ children }) {
+export function ButtonOperation({
+  handleOperation,
+  handleCalculate,
+  children,
+}) {
   const ButtonOperationStlye =
     'rounded-md w-12 h-14 sm:w-16 sm:h-16 text-center md:w-24 md:h-16  font-bold text-[24px] drop-shadow-md cursor hover:scale-110 bg-cyan-500 text-gray-50 translate transition-all '
-  return <button className={ButtonOperationStlye}>{children}</button>
+  return (
+    <>
+      {children === '=' ? (
+        <button
+          onClick={() => handleCalculate()}
+          className={ButtonOperationStlye}
+        >
+          {children}
+        </button>
+      ) : (
+        <button
+          onClick={() => handleOperation(children)}
+          className={ButtonOperationStlye}
+        >
+          {children}
+        </button>
+      )}
+    </>
+  )
 }
--- 01_calculator/src/components/Calculator.jsx
+++ 01_calculator/src/components/Calculator.jsx
@@ -9,7 +9,7 @@ export function Calculator() {
   const [completeOperation, setCompleteOperation] = useState('')
 
   const keyPadNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  const operations = ['+', '-', '*', '/']
+  const operations = ['+', '-', '*', '/', '=']
 
   function handleClick(val) {
     setCurrentValue((prevState) => {
@@ -23,6 +23,52 @@ export function Calculator() {
     setCompleteOperation((prevOperation) => prevOperation + val)
   }
 
+  function handleOperation(operation) {
+    setCompleteOperation(`${currentValue} ${operation}`)
+    setPendingOperation(operation)
+    setPendingValue(currentValue)
+    setCurrentValue('0')
+  }
+
+  function handleCalculate() {
+    if (!pendingOperation || !pendingValue) {
+      return ''
+    }
+
+    const numberOne = parseFloat(pendingValue)
+    const numberTwo = parseFloat(currentValue)
+
+    let result
+
+    switch (pendingOperation) {
+      case '+':
+        result = numberOne + numberTwo
+        break
+      case '-':
+        result = numberOne - numberTwo
+        break
+      case '*':
+        result = numberOne * numberTwo
+        break
+      case '/':
+        if (numberTwo !== 0) {
+          result = numberOne / numberTwo
+        } else {
+          setCurrentValue('Error')
+          setCompleteOperation('Error')
+          setPendingOperation(null)
+          setPendingValue(null)
+          return
+        }
+        break
+    }
+
+    setCompleteOperation((prevOperation) => prevOperation + ' = ' + result)
+    setCurrentValue(result.toString())
+    setPendingOperation(null)
+    setPendingValue(null)
+  }
+
   function handleClear() {
     setCurrentValue('0')
     setPendingOperation(null)
@@ -33,33 +79,34 @@ export function Calculator() {
   const displayCalculator =
     'border border-zinc-300 rounded-lg text-right py-6 px-4 text-lg font-bold overflow-x-auto text-zinc-800'
 
-  const displayFlexCol = 'flex flex-col gap-4'
   const buttonAcStyle =
     'rounded-md w-12 h-14 sm:w-16 sm:h-16 text-center md:w-24 md:h-16 font-bold text-[24px] drop-shadow-md cursor hover:scale-110 bg-cyan-500 text-gray-50 translate transition-all text-center'
 
   return (
-    <main className="max-w-[460px] flex flex-col gap-4">
+    <main className="max-w-[460px] flex flex-col gap-4 border border-zinc-300 p-4 rounded">
       <div className={displayCalculator}>
         <span className="text-zinc-500">{completeOperation}</span>
       </div>
       <div className={displayCalculator}>{currentValue}</div>
-      <div className="flex justify-between gap-1 mt-4">
-        <div className="flex gap-4 flex-wrap">
-          <button onClick={() => handleClear()} className={buttonAcStyle}>
-            AC
-          </button>
-          {keyPadNumbers.map((keypad) => (
-            <ButtonKeyPad handleClick={handleClick} key={keypad}>
-              {keypad}
-            </ButtonKeyPad>
-          ))}
-        </div>
+      <div className="grid grid-cols-4 gap-4 mt-4">
+        <button onClick={() => handleClear()} className={buttonAcStyle}>
+          AC
+        </button>
+        {keyPadNumbers.map((keypad) => (
+          <ButtonKeyPad handleClick={handleClick} key={keypad}>
+            {keypad}
+          </ButtonKeyPad>
+        ))}
 
-        <div className={displayFlexCol}>
-          {operations.map((keypad) => (
-            <ButtonOperation key={keypad}>{keypad}</ButtonOperation>
-          ))}
-        </div>
+        {operations.map((keypad) => (
+          <ButtonOperation
+            handleOperation={handleOperation}
+            handleCalculate={handleCalculate}
+            key={keypad}
+          >
+            {keypad}
+          </ButtonOperation>
+        ))}
       </div>
     </main>
   )
