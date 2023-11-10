392236fd44565c22a9c20e995f606b68833a4e7c
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-11-10 16:15:53 -0300

    sTYLE

--- 06_markdown_viewer/src/components/CardWithHtml/index.jsx
+++ 06_markdown_viewer/src/components/CardWithHtml/index.jsx
@@ -1,10 +1,16 @@
-export const CardWithHtml = () => {
+import { marked } from 'marked'
+
+export const CardWithHtml = ({ textToHtml }) => {
+  function renderText() {
+    return { __html: marked(textToHtml) }
+  }
   return (
     <>
       <h1 className="font-bold text-[24px] text-zinc-900">Markdown em HTML</h1>
-      <div className="flex flex-col text-gray-50 flex-1 grow gap-10 w-[100%] p-8 items-start bg-gray-950 rounded drop-shadow-md min-h-72 sm:max-w-[620px] md:max-w-[820px] lg:max-w-[1024px]">
-        a
-      </div>
+      <div
+        className="flex flex-col text-gray-50 flex-1 grow gap-10 w-[100%] p-8 items-start bg-gray-950 rounded drop-shadow-md min-h-72 sm:max-w-[620px] md:max-w-[820px] lg:max-w-[1024px] "
+        dangerouslySetInnerHTML={renderText()}
+      />
     </>
   )
 }
--- 06_markdown_viewer/src/components/MarkdownCardViewer/index.jsx
+++ 06_markdown_viewer/src/components/MarkdownCardViewer/index.jsx
@@ -3,7 +3,27 @@ import { Tollbar } from './Tollbar'
 import { CardWithHtml } from '../CardWithHtml'
 
 export const MarkdownCardViewer = () => {
-  const [text, setText] = useState('# Olá, eu sou feito de markdown')
+  const [text, setText] = useState(
+    localStorage.getItem('markdownText') ||
+      '# Digite aqui seu texto em markdown',
+  )
+  const [isHtmlActive, setIsHtmlActive] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('markdownText', text)
+  }, [text])
+
+  const textAreaRef = useRef(null)
+
+  function handleChangeText(e) {
+    setText(e.target.value)
+    setIsHtmlActive(true)
+
+    if (e.target.value === '') {
+      setIsHtmlActive(false)
+    }
+  }
+
   return (
     <>
       <div className="flex flex-col flex-1 grow gap-10 w-[100%] p-8 items-start bg-zinc-50 rounded mt-10 drop-shadow-md min-h-72 sm:max-w-[600px] md:max-w-[820px] lg:max-w-[1024px]">
@@ -11,8 +31,12 @@ export const MarkdownCardViewer = () => {
         <textarea
           rows="4"
           className="rounded-md  border-none outline-none w-[100%] bg-gray-950 text-gray-500 p-6 font-medium text-[18px] resize-none h-[360px]"
+          value={text}
+          ref={textAreaRef}
+          onChange={handleChangeText}
         ></textarea>
-        <CardWithHtml />
+
+        {isHtmlActive ? <CardWithHtml textToHtml={text} /> : ''}
       </div>
     </>
   )
